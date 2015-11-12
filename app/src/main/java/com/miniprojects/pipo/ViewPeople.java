9be8322d4f6199da8683510812f386ff10381a6d
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-12 20:51:23 +0300

    Activity 'ViewPeople' intitiated

--- app/src/main/java/com/miniprojects/pipo/ViewPeople.java
+++ app/src/main/java/com/miniprojects/pipo/ViewPeople.java
@@ -1,7 +1,15 @@
 package com.miniprojects.pipo;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by iWanjugu on 12/11/2015.
  */
-public class ViewPeople {
+public class ViewPeople extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_people);
+    }
 }
