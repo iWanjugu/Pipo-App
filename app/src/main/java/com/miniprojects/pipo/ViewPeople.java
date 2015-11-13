56b08d68d146ac030c8481af519f015f4337a0d0
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-13 03:04:01 +0300

    ViewPeople:  Layout - button created; Activity - OnClick listener on button

--- app/src/main/java/com/miniprojects/pipo/ViewPeople.java
+++ app/src/main/java/com/miniprojects/pipo/ViewPeople.java
@@ -1,15 +1,27 @@
 package com.miniprojects.pipo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-/**
- * Created by iWanjugu on 12/11/2015.
- */
+
 public class ViewPeople extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_people);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),PeopleDetails.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
